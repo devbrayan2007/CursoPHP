9a624ed2596f50f426c01bef17c1fbeba6b04260
devbrayan2007 <devbrayan2007@gmail.com>
2024-07-11 14:00:43 -0300

    atualização

--- Manipulação de Arquivos/arquivos.php
+++ Manipulação de Arquivos/arquivos.php
@@ -1,2 +1,17 @@
 <?php
 
+$arquivo = 'arquivo.txt';
+
+$arquivoAberto = fopen($arquivo,'r');
+$tamanhoArquivo = filesize($arquivo);
+
+$conteudo = "Teste\r\n";
+//fwrite($arquivoAberto, $conteudo);
+
+
+while(!feof($arquivoAberto)){
+    $linha = fgets($arquivoAberto, $tamanhoArquivo);
+    echo $linha . "<br>";
+}
+
+fclose($arquivoAberto);
